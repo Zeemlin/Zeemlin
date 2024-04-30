b8017ae46c24250ab72114b017d951c106ccef61
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-05-01 01:30:04 +0500

    Description is optional

--- Zeemlin.Domain/Entities/Lesson.cs
+++ Zeemlin.Domain/Entities/Lesson.cs
@@ -8,7 +8,7 @@ namespace Zeemlin.Domain.Entities;
 public class Lesson : Auditable
 {
     public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public string StartDate { get; set; }
     public string EndDate { get; set; }
     public long GroupId { get; set; }
