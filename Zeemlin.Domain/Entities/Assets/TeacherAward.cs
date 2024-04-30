84146341b4be3f71b3056c7b4ce654de2fb4f0dc
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-05-01 01:25:01 +0500

    A class has been created to upload Teacher Certificates

---
+++ Zeemlin.Domain/Entities/Assets/TeacherAward.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Domain.Commons;
+using Zeemlin.Domain.Entities.Users;
+
+namespace Zeemlin.Domain.Entities.Assets;
+
+public class TeacherAward : Auditable
+{
+    public string Path { get; set; }
+    public DateTime UploadedDate { get; set; }
+    public long TeacherId { get; set; }
+    public Teacher Teacher { get; set; }
+}
