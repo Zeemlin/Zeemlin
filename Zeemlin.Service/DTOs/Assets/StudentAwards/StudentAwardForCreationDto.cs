80a735a3c055b6dc558d120757d642bd6ce42c0b
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-05-03 18:32:45 +0500

    Student Award DTOs and Services are done

---
+++ Zeemlin.Service/DTOs/Assets/StudentAwards/StudentAwardForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Zeemlin.Service.DTOs.Assets.StudentAwards;
+
+public class StudentAwardForCreationDto
+{
+    public IFormFile Path { get; set; }
+    public long StudentId { get; set; }
+}
---
+++ Zeemlin.Service/Interfaces/Assets/IStudentAwardService.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Service.Configurations;
+using Zeemlin.Service.DTOs.Assets.StudentAwards;
+
+namespace Zeemlin.Service.Interfaces.Assets;
+
+public interface IStudentAwardService
+{
+    Task<bool> DeletePictureAsync(long Id);
+    Task<StudentAwardForResultDto> RetrieveByIdAsync(long Id);
+    Task<StudentAwardForResultDto> UploadAsync(StudentAwardForCreationDto dto);
+    Task<IEnumerable<StudentAwardForResultDto>> RetrieveAllAsync(PaginationParams @params);
+}
