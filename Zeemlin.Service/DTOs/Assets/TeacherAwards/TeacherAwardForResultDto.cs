ad3beca547ad0c733a3f7d865ed0e61f09bfc48a
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-05-08 16:56:13 +0500

    API ready for TeacherAwards

---
+++ Zeemlin.Service/DTOs/Assets/TeacherAwards/TeacherAwardForResultDto.cs
@@ -0,0 +1,12 @@
+﻿using Zeemlin.Service.DTOs.Users.Teachers;
+
+namespace Zeemlin.Service.DTOs.Assets.TeacherAwards;
+
+public class TeacherAwardForResultDto
+{
+    public long Id { get; set; }
+    public string Path { get; set; }
+    public DateTime UploadedDate { get; set; }
+    public long TeacherId { get; set; }
+    public TeacherForResultDto Teacher { get; set; }
+}
